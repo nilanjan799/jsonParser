93545f98e2cd9d3b047a9e68fa9c99b639161967
Nilanjan Saha <nilanjan71299@gmail.com>
2024-10-28 11:01:39 +0530

    parser

--- src/app.service.ts
+++ src/app.service.ts
@@ -130,4 +130,107 @@ export class AppService {
     
     return {token: {type: 'Number', value: parseFloat(numstring)}, postion: pos};
   }
+
+  parse(json: string): any{
+    const tokens = this.tokenize(json);
+
+  }
+
+  private parseValue(tokens: Token[], index: number): any{
+    const token = tokens[index];
+
+    switch(token.type){
+      case 'String': 
+        return this.processStringToken(tokens, index);
+      case 'Number':
+        return this.processNumToken(tokens, index);
+      case 'Boolean':
+        return this.processBoolToken(tokens, index);
+      case 'Null':
+        return {value: null, index: index+1};
+      case 'LeftBrace':
+        return this.processObject(tokens, index);
+      case 'LeftBracket':
+        return this.processArray(tokens, index);
+      default: 
+        throw new Error('unexpected token');
+    }
+  }
+
+  private processStringToken(tokens: Token[], index: number): {value: string, index: number}{
+    const token = tokens[index] as {type: 'String', value: string};
+    return {value: token.value, index: index+1};
+  }
+
+  private processNumToken(tokens:  Token[], index: number): {value: number, index: number}{
+    const token = tokens[index] as {type: 'Number', value: number};
+    return {value: token.value, index: index+1};
+  }
+
+  private processBoolToken(tokens: Token[], index: number): {value: boolean, index: number}{
+    const token = tokens[index] as {type: 'Boolean', value: boolean};
+    return {value: token.value, index: index+1};
+  }
+
+  private processObject(tokens: Token[], index: number): {value: Record<string, any>, index: number}{
+    const result: Record<string, any> = {};
+    let currentIndex = index+1; //ignore the leftBrace token
+
+    while(currentIndex < tokens.length){
+      const fieldName = this.processStringToken(tokens, currentIndex);
+      currentIndex = fieldName.index;
+
+      if(currentIndex >= tokens.length || tokens[currentIndex].type != 'Colon') throw new SyntaxError('error in json');      
+      currentIndex++;
+
+      const fieldValue = this.parseValue(tokens, currentIndex);
+      result[fieldName.value] = fieldValue.value;
+      currentIndex = fieldValue.index;
+
+      if(tokens[currentIndex].type === 'Comma') currentIndex++;
+      else if(tokens[currentIndex].type === 'RightBrace') return {value: result, index: currentIndex+1};
+      else throw new SyntaxError(`unexpected token at ${currentIndex}`);
+    }
+    throw new SyntaxError('error in json');
+  }
+
+  private processArray(tokens: Token[], index: number): {value: any[], index: number}{
+    const result: any[] = [];
+    let expectValue = true;
+
+    while(index < tokens.length){
+      const currToken = tokens[index];
+
+      if(currToken.type === 'RightBracket'){
+        if(expectValue && result.length > 0){
+          throw new SyntaxError('error in the array syntax');
+        }        
+        return {value: result, index: index+1};
+      }
+
+      if(currToken.type === 'Comma'){
+        if(expectValue || result.length === 0){
+          throw new SyntaxError('error in array syntax');
+        }
+        expectValue = true;
+        index++;
+        continue;
+      }
+
+      if(expectValue){
+        const resultValue = this.parseValue(tokens, index);
+        result.push(resultValue.value);
+        index++;
+        expectValue = false;
+
+        if(tokens[index]?.type != 'Comma' && tokens[index]?.type != 'RightBracket'){
+          throw new SyntaxError('error in array syntax');
+        }
+      }
+      else throw new SyntaxError('error!');
+    }
+
+    throw new SyntaxError('error!');
+  }
+
 }
